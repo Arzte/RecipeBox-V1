44c9f7a64ae8592fbf68bd599358d470e14a92c3
Kyle Negley <kpnegley@yahoo.com>
2020-09-11 10:21:57 -0400

    got recipe_detail to work and show favorites and to be able to have the author of a recipe or the admin edit a recipe

--- homepage/models.py
+++ homepage/models.py
@@ -20,6 +20,9 @@ class Author(models.Model):
     name = models.CharField(max_length=80)
     bio = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_recipe = models.ManyToManyField(to="Recipe",
+                                             related_name="Recipe_Favorites",
+                                             symmetrical=False)
 
     def __str__(self):
         return self.name
@@ -34,3 +37,13 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.author.name}"
+
+
+# class Favorite(models.Model):
+#     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+#     is_favorite = models.BooleanField(default=False)
+#     user_favorite = models.CharField(max_length=80)
+
+#     def __str__(self):
+#         return f"{self.recipe} - {self.is_favorite}"
--- homepage/views.py
+++ homepage/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 from homepage.models import Author, Recipe
 from homepage.forms import AddRecipeForm, AddAuthorForm, LoginForm
 
@@ -17,16 +17,28 @@ def index(request):
 
 
 def recipe_detail(request, recipe_id):
-    recipe = Recipe.objects.filter(id=recipe_id).first()
-    return render(request, 'recipe_detail.html', {"recipe": recipe})
+    try:
+        recipe = Recipe.objects.filter(id=recipe_id).first()
+        favorites = [f for f in Author.objects.get(
+            user=request.user).favorite_recipe.all()]
+
+    except Exception:
+        favorites = None
+
+    return render(request, 'recipe_detail.html', {"recipe": recipe, "favorites": favorites})
 
 
 def author_detail(request, author_id):
     author = Author.objects.filter(id=author_id).first()
-    recipes = Recipe.objects.filter(author=author.id).all()
+    recipes = Recipe.objects.filter(author=author.id)
+
+    favorites = [f for f in Author.objects.get(
+        id=author_id).favorite_recipe.all()]
+
     return render(request, 'author_detail.html', {
         "author": author,
-        "recipes": recipes
+        "recipes": recipes,
+        "favorites": favorites or None
     })
 
 
@@ -50,6 +62,32 @@ def add_recipe(request):
         "form": form,
         "name": "recipe"
     })
+
+
+@login_required
+def edit_recipe(request, recipe_id):
+    try:
+        recipe = Recipe.objects.get(id=recipe_id)
+        initial_data = recipe.__dict__
+
+        form = AddRecipeForm(initial=initial_data)
+        if request.method == "POST":
+
+            form = AddRecipeForm(request.POST, initial=initial_data)
+
+            if form.is_valid() and form.has_changed():
+                data = form.cleaned_data
+
+                for field, data in data.items():
+                    setattr(recipe, field, data)
+
+                recipe.save()
+
+            return HttpResponseRedirect(reverse("recipedetail", args=[recipe_id]))
+
+    except Exception:
+        return HttpResponseRedirect(reverse("homepage"))
+    return render(request, "recipe_form.html", {"form": form})
 
 
 @login_required
@@ -101,3 +139,23 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("homepage"))
+
+
+@login_required
+def add_favorite(request, recipe_id):
+    try:
+        recipe = Recipe.objects.get(id=recipe_id)
+        author = request.user.author
+        favorite = Author.objects.get(
+            user=request.user).favorite_recipe.get(id=recipe_id)
+        # breakpoint()
+    except ObjectDoesNotExist:
+        favorite = None
+    # breakpoint()
+    if not favorite:
+        author.favorite_recipe.add(recipe)
+        return HttpResponseRedirect(reverse("recipedetail", args=[recipe_id]))
+
+    author.favorite_recipe.remove(recipe)
+
+    return HttpResponseRedirect(reverse("recipedetail", args=[recipe_id]))
